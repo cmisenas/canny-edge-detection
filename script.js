52ee670eb0a5798165b84f8194837903ee8b8da5
cmisenas <acolyte.camille@gmail.com>
2013-07-09 17:21:20 -0400

    Added an edge thinning function

--- script.js
+++ script.js
@@ -22,10 +22,17 @@
 	//load image data
 	ctx.drawImage(img, 0, 0);
 	
+	var clicks = 0;
 	window.addEventListener('click', function(){
 		imgData = ctx.getImageData(0, 0, W, H);
-		var newImgData = edgeDetect(imgData);
-		ctx.putImageData(newImgData, 0, 0);
+		if(clicks === 0){
+			var newImgData = edgeDetect(imgData);
+			ctx.putImageData(newImgData, 0, 0);
+		}else if(clicks === 1){
+			var newerImgData = edgeThin(imgData);
+			ctx.putImageData(newerImgData, 0, 0);
+		}
+		clicks++;
 	});
 	
 	function edgeDetect(imgData){
@@ -41,6 +48,56 @@
 					setPixel(i, 255, imgDataCopy);
 				}else{
 					setPixel(i, 0, imgDataCopy);
+				}
+			}
+		}
+		return imgDataCopy;
+	}
+
+	function edgeThin(imgData){//2-pass algorithm
+		//iterate through each pixel and do an 3x3 search on the neighbors
+		//mark if
+		//*it has no neighbors
+		//*it's nw, n, ne || nw, w, sw || sw, s, se || ne, e, se || nw, n, e || ne, n, w || nw, w, s || sw, w, n || w, s, se, || e, s, sw || n, e, se || s, e, ne is filled
+		//	___________________
+		//	|	nw	|	 n	|	ne	|
+		//	|_____|_____|_____|
+		//	|	 w	|	 o	|	 e	|
+		//	|_____|_____|_____|
+		//	|	sw	|	 s	|	se	|
+		//	|_____|_____|_____|
+		var imgDataCopy = copyImageData(ctx, imgData);
+		for(var y = 0; y < imgData.height; y++){
+			for(var x = 0; x < imgData.width; x++){
+				var i = x * 4 + y * imgData.width * 4;
+				var o = imgDataCopy.data[i];
+				var n = imgDataCopy.data[i - imgData.width * 4];
+				var nw = imgDataCopy.data[i - imgData.width * 4 - 4];
+				var ne = imgDataCopy.data[i - imgData.width * 4 + 4];
+				var w = imgDataCopy.data[i - 4];
+				var e = imgDataCopy.data[i + 4];
+				var s = imgDataCopy.data[i + imgData.width * 4];
+				var sw = imgDataCopy.data[i + imgData.width * 4 - 4];
+				var se = imgDataCopy.data[i + imgData.width * 4 + 4];
+				if(((n && nw && ne && w && e && s && sw && se) ||
+					(!nw && !n && !ne) ||
+					(!nw && !w && !sw) ||
+					(!sw && !s && !se) ||
+					(!ne && !e && !se) ||
+					(!nw && !n && !e) ||
+					(!ne && !n && !w) ||
+					(!nw && !w && !s) ||
+					(!sw && !w && !n) ||
+					(!w && !s && !se) ||
+					(!e && !s && !sw) ||
+					(!n && !e && !se) ||
+					(!s && !e && !ne) ||
+					(!w && !nw && !n) ||
+					(!n && !ne && !e) ||
+					(!e && !se && !s) ||
+					(!w && !sw && !s)) &&
+					(!o)){
+					setPixel(i, 255, imgDataCopy);
 				}
 			}
 		}
